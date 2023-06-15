6da93a36ee8d292d1788e4ccdb5e4f7a6b358632
juffalx <juffalx@gmail.com>
2023-06-15 14:00:25 -0700

    4 and 5 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * this file is focus on display by using puts
- * the return value is 0 
- * and other's 
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

1a94553ee8416c3e645dad12b13fa232a30e87f8
juffalx <juffalx@gmail.com>
2023-06-20 10:25:16 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - putchar display
+ *
+ * Treturn 0 Always (success)
+ *
+ */
+
+int main (void)
+{
+	putchar('_putchar');
+	putchar('\n');
+return (0);
+}

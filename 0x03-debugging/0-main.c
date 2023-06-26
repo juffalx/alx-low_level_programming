9daf566fc220c99a20bb35be4e054e4e6c7a04ee
juffalx <juffalx@gmail.com>
2023-06-26 03:22:34 -0700

    positive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -test function that print if negative or possitive num
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int  i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}

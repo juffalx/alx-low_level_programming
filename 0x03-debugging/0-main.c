2a26afede7938d1ce1a6b8b22b8e4b2d858b2b80
juffalx <juffalx@gmail.com>
2023-06-26 03:24:47 -0700

    negative or possitve

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,13 @@
 
 /**
  * main -test function that print if negative or possitive num
+ *
  * Return: 0 always
  */
 
 int main(void)
 {
-	int  i;
-
-	i = 0;
+	int  i = 0;
 
 	positive_or_negative(i);
 

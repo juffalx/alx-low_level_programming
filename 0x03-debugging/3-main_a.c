214f18b0c0c443d0eaca809777675d2fda5603a2
juffalx <juffalx@gmail.com>
2023-06-27 14:20:34 -0700

    update

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -9,19 +9,19 @@
 
 int main(void)
 {
-    int month;
-    int day;
-    int year;
+	int month;
+	int day;
+	int year;
 
-    month = 4;
-    day = 1;
-    year = 1997;
+	month = 4;
+	day = 1;
+	year = 1997;
 
-    printf("Date: %d/%d/%d\n", month, day, year);
+	printf("Date: %d/%d/%d\n", month, day, year);
 
-    day = convert_day(month, day);
+	day = convert_day(month, day);
 
-    print_remaining_days(month, day, year);
+	print_remaining_days(month, day, year);
 
-    return (0);
+	return (0);
 }

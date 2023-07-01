f06757fde9983f6f05db1c5fe73b6a3196b17296
juffalx <juffalx@gmail.com>
2023-07-01 14:24:27 -0700

    update 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,16 +13,16 @@ int _strcmp(char *s1, char *s2)
 	int i, j;
 
 	i = 0;
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s1[i] != '\0')
 	{
-		i++;
+		if (s1[i] != s1[i])
+		{
+			return (s1[i] - s2[i]);
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
 	}
-
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-
-	return ( i - j);
 }

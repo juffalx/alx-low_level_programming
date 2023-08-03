918676f14bb25a2fb6ee54761929eb3af3db3135
MOHAMMED YASIN <132158698+juffalx@users.noreply.github.com>
2023-08-03 07:10:19 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef function_like_macro_h
-#define function_like_macro_h
-
-#define ABS(num) ((num) < (0) ? -(num) : (num))
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef sum_h
-#define sum_h
-
-#define SUM(num1, num2) (num1 + num2)
-
-#endif

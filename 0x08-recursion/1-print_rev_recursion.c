d5c78a3bd4937f75f22bdf21d48318049d5cf9a6
juffalx <juffalx@gmail.com>
2024-12-29 03:47:08 -0800

    recursion 0x08 project first 1 reverse like pust project updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,13 +10,13 @@
 
 void _print_rev_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                return;
+	if (*s == '\0')
+	{
+		return;
 
-        }
+	}
 
 
-        _print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }

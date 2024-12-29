80ab426a31466de3721c74928d62d21abcfea83d
juffalx <juffalx@gmail.com>
2024-12-29 03:15:11 -0800

    recursion 0x08 project first 0 like pust project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function return input string
+ * *s: is parameter
+ * Return: it's value
+ */
+
+
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+ it;s all about recursion 0x08
